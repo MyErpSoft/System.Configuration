42009d86defb5df55dfba676bde0e178f9be2823
myerpsoft <github@myerpsoft.com>
2016-04-19 15:10:02 +0800

    Providers对外实现支持并发安全。

--- src/System.Configuration.Core/ConfigurationRuntime.cs
+++ src/System.Configuration.Core/ConfigurationRuntime.cs
@@ -24,10 +24,10 @@ namespace System.Configuration.Core {
                 Utilities.ThrowArgumentNull(nameof(binder));
             }
             this._binder = binder;
-            this._packageProviders = new IPackageProvider[] {
+            this._providers = new PackageProviderCollection(this,
                 new Dcxml.DcxmlPackageProvider(),
                 new Dc.DcPackageProvider()
-            };
+            );
         }
 
         private readonly ConfigurationObjectBinder _binder;
@@ -38,12 +38,68 @@ namespace System.Configuration.Core {
             get { return _binder; }
         }
 
-        private readonly IPackageProvider[] _packageProviders;
+        private IPackageProvider[] _packageProviders;
+        //内部使用，数组循环更快，但不安全。
+        internal IPackageProvider[] GetProviders() {
+            return _packageProviders;
+        }
+
+        private readonly PackageProviderCollection _providers;
         /// <summary>
         /// 返回当前所有可用的PackageProvider，由于搜索Package,默认初始化了内置的Dcxml和Dc两种格式。
         /// </summary>
-        public IPackageProvider[] GetProviders() {
-            return _packageProviders;//todo 需要copy
+        public Collection<IPackageProvider> PackageProviders {
+            get { return _providers; }
         }
+        #region PackageProviderCollection
+        /// <summary>
+        /// 处理runtime并发问题的代码，外界修改provider
+        /// </summary>
+        private sealed class PackageProviderCollection : Collection<IPackageProvider> {
+            private readonly ConfigurationRuntime _runtime;
+
+            public PackageProviderCollection(ConfigurationRuntime runtime, params IPackageProvider[] providers) {
+                this._runtime = runtime;
+                runtime._packageProviders = providers;
+                foreach (var item in providers) {
+                    Items.Add(item);
+                }
+            }
+
+            protected override void InsertItem(int index, IPackageProvider item) {
+                lock (this) {
+                    base.InsertItem(index, item);
+                    this.Update();
+                }
+            }
+
+            protected override void RemoveItem(int index) {
+                lock (this) {
+                    base.RemoveItem(index);
+                    this.Update();
+                }
+            }
+
+            protected override void SetItem(int index, IPackageProvider item) {
+                lock (this) {
+                    base.SetItem(index, item);
+                    this.Update();
+                }
+            }
+
+            protected override void ClearItems() {
+                lock (this) {
+                    base.ClearItems();
+                    this.Update();
+                }
+            }
+
+            private void Update() {
+                IPackageProvider[] newArray = new IPackageProvider[this.Count];
+                this.CopyTo(newArray, 0);
+                _runtime._packageProviders = newArray;
+            }
+        } 
+        #endregion
     }
 }
